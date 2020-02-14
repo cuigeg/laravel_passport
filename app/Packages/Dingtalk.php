dc583826727a9f6d05d73e67c1d73ba0aa990511
lxl <2998604835@qq.com>
2020-02-14 18:01:24 +0800

    '获取用户id'

--- app/Packages/Dingtalk.php
+++ app/Packages/Dingtalk.php
@@ -2,6 +2,7 @@
 
 namespace App\Packages;
 
+use GuzzleHttp\Client;
 use Illuminate\Database\Eloquent\Model;
 
 class Dingtalk extends Model
@@ -11,16 +12,10 @@ class Dingtalk extends Model
         if (cache('access_token')) {
             return cache('access_token');
         }
-
-        $client = new \DingTalkClient(\DingTalkConstant::$CALL_TYPE_TOP, \DingTalkConstant::$METHOD_GET, \DingTalkConstant::$FORMAT_JSON);
-        $request = new \OapiGettokenRequest();
-        $request->setAppkey(env('DINGTALK_APPKEY'));
-        $request->setAppsecret(env('DINGTALK_APPSECRET'));
-        $res = $client->execute($request);
-        dd($res);
-        file_put_contents('dingtalk.txt', 'get access token 返回值：' . PHP_EOL, FILE_APPEND);
-        file_put_contents('dingtalk.txt', json_encode($res) . PHP_EOL, FILE_APPEND);
-        if ($res->errcode == 0) {
+        $http = new Client();
+        $response = $http->get('https://oapi.dingtalk.com/gettoken?appkey=' . env('DINGTALK_APPKEY').'&appsecret=' . env('DINGTALK_APPSECRET'));
+        $res = json_decode((string)$response->getBody());
+        if (isset($res->errcode) && $res->errcode == 0) {
             cache(['access_token' => $res->access_token], 120);
             file_put_contents('dingtalk.txt', 'getAccessToken返回值：' . PHP_EOL, FILE_APPEND);
             file_put_contents('dingtalk.txt', $res->access_token . PHP_EOL, FILE_APPEND);
@@ -28,18 +23,35 @@ class Dingtalk extends Model
         } else {
             return false;
         }
+
+//        $client = new \DingTalkClient(\DingTalkConstant::$CALL_TYPE_TOP, \DingTalkConstant::$METHOD_GET, \DingTalkConstant::$FORMAT_JSON);
+//        $request = new \OapiGettokenRequest();
+//        $request->setAppkey(env('DINGTALK_APPKEY'));
+//        $request->setAppsecret(env('DINGTALK_APPSECRET'));
+//        $res = $client->execute($request);
+//        dd($res);
+//        file_put_contents('dingtalk.txt', 'get access token 返回值：' . PHP_EOL, FILE_APPEND);
+//        file_put_contents('dingtalk.txt', json_encode($res) . PHP_EOL, FILE_APPEND);
+//        if ($res->errcode == 0) {
+//            cache(['access_token' => $res->access_token], 120);
+//            file_put_contents('dingtalk.txt', 'getAccessToken返回值：' . PHP_EOL, FILE_APPEND);
+//            file_put_contents('dingtalk.txt', $res->access_token . PHP_EOL, FILE_APPEND);
+//            return $res->access_token;
+//        } else {
+//            return false;
+//        }
     }
 
     public static function getUserId($code)
     {
         $access_token = self::getAccessToken();
-        $client = new \DingTalkClient(\DingTalkConstant::$CALL_TYPE_OAPI,\DingTalkConstant::$METHOD_GET,\DingTalkConstant::$FORMAT_JSON);
+        $client = new \DingTalkClient(\DingTalkConstant::$CALL_TYPE_TOP,\DingTalkConstant::$METHOD_GET,\DingTalkConstant::$FORMAT_JSON);
         $request = new \OapiUserGetuserinfoRequest();
         $request->setCode($code);
         $res = $client->execute($request,$access_token);
         file_put_contents('dingtalk.txt', 'getUserId 返回值：' . PHP_EOL, FILE_APPEND);
         file_put_contents('dingtalk.txt', json_encode($res) . PHP_EOL, FILE_APPEND);
-        if ($res->errcode == 0) {
+        if (isset($res->errcode) && $res->errcode == 0) {
             return $res;
         } else {
             return false;
